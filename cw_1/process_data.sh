0a19285751979f9116dffb8156dfe81c66108112
Corwin Ansley 2024/25 Bioinformatics Student <2266643a@headnode04.hpc.gla.ac.uk>
2025-03-16 15:43:57 +0000

    started process data script

---
+++ cw_1/process_data.sh
@@ -0,0 +1,57 @@
+#! /bin/bash 
+#PBS -l nodes=1:ppn=1:centos7,cput=24:00:00,walltime=48:00:00 
+#PBS -N process_data
+#PBS -d /export/biostuds/2266643a/BIOL5177_rnaseq/Practicals/practical_2/nondiscovery 
+#PBS -m abe 
+#PBS -M 2266643a@student.gla.ac.uk 
+#PBS -q bioinf-stud
+
+# RESOURCES
+adapter=illumina_adapter.fa # path to adapter
+hs2index="./Hisat2Index/dm3"         # path to reference genome hisat2 indexes 
+gtf=dmel-all-no-analysis-r5.57clean2.gtf    	         # path to GTF file 
+data=./data             # path to local data directory that you have created 
+
+# <Make subdirs
+hisat_dir=./hisat_results
+stringtie_dir=./stringtie_results
+mkdir -p $hisat_dir
+mkdir -p $stringtie_dir
+
+gtflist='list.gtf.txt' # filename for a final GTF list 
+
+rm -f ${gtflist}            # remove if exists 
+
+# Running in loop to carry out work
+for sample in tb1 tb2 tb3 wf1 wf2 wf3
+
+do
+    fastq="${data}/${sample}.fq"	# path to raw fastq file 
+    trim1="$data/${sample}.t1.fq"		# path to adapter-trimmed fastq file 
+    trim2="$data/${sample}.t2.fq"		# path to quality-trimmed fastq file 
+    sam="${hisat_dir}/${sample}.sam"  # path to hisat2-generated SAM file
+    bam="${hisat_dir}/${sample}.bam"  # path to samtools-converted BAM file 
+    sorted_bam="${hisat_dir}/${sample}.sort.bam"	 # path to samtools-sorted BAM file    
+    
+    
+    scythe -o $trim1 -a illumina_adapter.fa -q illumina ${fastq}
+    sickle se -f $trim1 -t illumina -o $trim2  -q 10 -l 51
+    hisat2 -p 4 --phred64 -x ${hs2index} -U ${trim2} -S ${sam} # command to execute hisat2 
+    samtools view -b -o ${bam} ${sam}		# command to execute samtools view 
+    samtools sort -o ${sorted_bam} ${bam}	# command to execute samtools sort 
+
+    rm ${sam} ${bam}		# removing unnecessary SAM/BAM files 
+    rm ${trim1} ${trim2}	# removing unnecessary trimmed FASTQ files 
+
+    str_smp_dir="${stringtie_dir}/${sample}"		# path to sample-specific subdirectory for stringtie results 
+
+    mkdir -p $str_smp_dir	# make the above directory 
+    sample_tr_gtf="${str_smp_dir}/${sample}_transcripts.gtf"
+    stringtie -p 4 -t -e -B -G ${gtf} -o ${sample_tr_gtf} ${sorted_bam}	# command to execute stringtie 
+
+    gtfline="${sample} $sample_tr_gtf" # line containing sample and path to string tie-generated GTF file 
+    echo ${gtfline} >> ${gtflist}	# adding the above line to a file 
+
+done 
+
+prepDE.py -i ${gtflist}
