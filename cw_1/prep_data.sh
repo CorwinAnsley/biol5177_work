7d28214f6f2c203358e031443561c50b48bc36f4
Corwin Ansley 2024/25 Bioinformatics Student <2266643a@headnode04.hpc.gla.ac.uk>
2025-03-19 03:39:05 +0000

    fixed data prep script

--- cw_1/prep_data.sh
+++ cw_1/prep_data.sh
@@ -10,10 +10,10 @@ fqpath=/export/home/gmh5n/assignment
 for sample in s1 s1t s2 s2t s3 s4 s5 s6 s7 s8 s9 s10 s11 s12
 
 do
-    raw_file=$fqpath/${sample}.fq  # path to the raw fast file 
-    link_name=/export/biostuds/2266643a/BIOL5177_rnaseq/biol5177_work/cw_1/data/${sample}.fq     # link-name to appear in your directory 
+    raw_file=$fqpath/${sample}.c2.fq  # path to the raw fast file 
+    link_name=/export/biostuds/2266643a/BIOL5177_rnaseq/biol5177_work/cw_1/data/${sample}.c2.fq     # link-name to appear in your directory 
 
     ln -s $raw_file $link_name             # instruction to make a soft link 
-    test_file=/export/biostuds/2266643a/BIOL5177_rnaseq/biol5177_work/cw_1/data/${sample}.25K.fq       # test fast file containing 25K reads 
+    test_file=/export/biostuds/2266643a/BIOL5177_rnaseq/biol5177_work/cw_1/data/${sample}.c2.25K.fq       # test fast file containing 25K reads 
     head -n 100000  $link_name > $test_file       # instruction to generate test file 
 done 
