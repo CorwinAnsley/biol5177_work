272924624705cf55249076ed7a0f782973afcdef
Corwin Ansley 2024/25 Bioinformatics Student <2266643a@headnode04.hpc.gla.ac.uk>
2025-03-19 04:58:52 +0000

    working on process data script

--- cw_1/process_data.sh
+++ cw_1/process_data.sh
@@ -1,15 +1,15 @@
 #! /bin/bash 
 #PBS -l nodes=1:ppn=1:centos7,cput=24:00:00,walltime=48:00:00 
 #PBS -N process_data
-#PBS -d /export/biostuds/2266643a/BIOL5177_rnaseq/Practicals/practical_2/nondiscovery 
+#PBS -d /export/biostuds/2266643a/BIOL5177_rnaseq/biol5177_work/cw_1
 #PBS -m abe 
 #PBS -M 2266643a@student.gla.ac.uk 
 #PBS -q bioinf-stud
 
 # RESOURCES
 adapter=illumina_adapter.fa # path to adapter
-hs2index="./Hisat2Index/dm3"         # path to reference genome hisat2 indexes 
-gtf=dmel-all-no-analysis-r5.57clean2.gtf    	         # path to GTF file 
+hs2index="/export/projects/polyomics/Genome/Mus_musculus/mm10/Hisat2Index/chr2"         # path to reference genome hisat2 indexes 
+gtf=/export/projects/polyomics/Genome/Mus_musculus/mm10/annotations/chr2.gtf   	         # path to GTF file 
 data=./data             # path to local data directory that you have created 
 
 # <Make subdirs
@@ -23,9 +23,10 @@ gtflist='list.gtf.txt' # filename for a final GTF list
 rm -f ${gtflist}            # remove if exists 
 
 # Running in loop to carry out work
-for sample in tb1 tb2 tb3 wf1 wf2 wf3
+for sample in s1 s1t s2 s2t s3 s4 s5 s6 s7 s8 s9 s10 s11 s12
 
 do
+    sample="${sample}.c2.25K"
     fastq="${data}/${sample}.fq"	# path to raw fastq file 
     trim1="$data/${sample}.t1.fq"		# path to adapter-trimmed fastq file 
     trim2="$data/${sample}.t2.fq"		# path to quality-trimmed fastq file 
@@ -34,9 +35,9 @@ do
     sorted_bam="${hisat_dir}/${sample}.sort.bam"	 # path to samtools-sorted BAM file    
     
     
-    scythe -o $trim1 -a illumina_adapter.fa -q illumina ${fastq}
-    sickle se -f $trim1 -t illumina -o $trim2  -q 10 -l 51
-    hisat2 -p 4 --phred64 -x ${hs2index} -U ${trim2} -S ${sam} # command to execute hisat2 
+    scythe -o $trim1 -a illumina_adapter.fa -q sanger ${fastq}
+    sickle se -f $trim1 -t sanger -o $trim2  -q 10 -l 51
+    hisat2 -p 4 --phred33 -x ${hs2index} -U ${trim2} -S ${sam} # command to execute hisat2 
     samtools view -b -o ${bam} ${sam}		# command to execute samtools view 
     samtools sort -o ${sorted_bam} ${bam}	# command to execute samtools sort 
 
