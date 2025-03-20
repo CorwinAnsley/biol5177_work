503f10529f10dd3c42481c881cf13992f96fe112
unknown <coreyansley@gmail.com>
2025-03-20 16:59:01 +0000

    working on cw script

--- cw_1/cw_de.R
+++ cw_1/cw_de.R
@@ -1,15 +1,16 @@
 
 
-# Install Dependencies (uncomment if not installed)
-#install.packages("BiocManager") 
-#BiocManager::install("DESeq2")
-#BiocManager::install("limma")
+# Install dependencies (uncomment if not installed)
+install.packages("BiocManager") 
+BiocManager::install("DESeq2")
+BiocManager::install("limma")
 
 # Load required packages
 library(DESeq2)
 library(ggplot2)
 library("limma")
 
+# Load in and format data
 gene_count_table = read.csv("./data/gene_count_matrix.csv",row.names=1)
 tran_count_table = read.csv("./data/transcript_count_matrix.csv",row.names=1)
 
@@ -20,3 +21,28 @@ sample_names = c('s1','s10','s11','s12','s2','s3','s4','s5','s6','s7','s8','s9')
 
 colnames(gene_count_table) = sample_names
 colnames(tran_count_table) = sample_names
+
+col_table = read.csv("./data/Design-Exp.csv",row.names=1)
+
+# Create dds object
+dds_genes = DESeqDataSetFromMatrix(countData=gene_count_table,colData=col_table,design= ~ group)
+dds_genes
+
+dds_tran = DESeqDataSetFromMatrix(countData=tran_count_table,colData=col_table,design= ~ group)
+dds_tran
+
+# Carry out pre-filtering
+
+min_group_size = 3
+keep = rowSums(counts(dds_genes) >= 10) >= min_group_size
+dds_genes = dds_genes[keep,]
+
+keep = rowSums(counts(dds_tran) >= 10) >= min_group_size
+dds_tran = dds_tran[keep,]
+
+# Run the DE analysis 
+dds_genes = DESeq(dds_genes)
+
+plotDispEsts(dds_genes)
+
+
